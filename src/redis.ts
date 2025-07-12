62772df45b83fd0db39280750df91537a57b1c1d
HrithikSampson <hrithikedwardsampson@gmail.com>
2025-07-12 19:07:36 +0530

    redis url host name change

--- src/redis.ts
+++ src/redis.ts
@@ -3,13 +3,13 @@ import { createClient } from 'redis';
 import { RedisOptions } from 'bullmq';
 import dotenv from "dotenv";
 dotenv.config();
-const redisUrl = process.env.REDIS_URL;
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 const redisPort = parseInt(process.env.REDIS_PORT) || 6379;
 export const pubClient = createClient({ url: `${redisUrl}:${redisPort}` });
 export const subClient = pubClient.duplicate();
 
 export const bullmqConnection = {
-  host: 'localhost',
+  host: redisUrl,
   port: redisPort,
 } satisfies RedisOptions;
 
