6d932a5829a8800debded49a6a2c2eec4a86962c
HrithikSampson <hrithikedwardsampson@gmail.com>
2025-07-12 19:02:36 +0530

    fix: redis url

--- src/redis.ts
+++ src/redis.ts
@@ -3,7 +3,7 @@ import { createClient } from 'redis';
 import { RedisOptions } from 'bullmq';
 import dotenv from "dotenv";
 dotenv.config();
-const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+const redisUrl = process.env.REDIS_URL;
 const redisPort = parseInt(process.env.REDIS_PORT) || 6379;
 export const pubClient = createClient({ url: `${redisUrl}:${redisPort}` });
 export const subClient = pubClient.duplicate();
